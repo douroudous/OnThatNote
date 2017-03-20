0b8c792c08e11f9b25d82896f138f4e4987bf292
Dino Dourountoudakis <CDOUROUNTOUDAKIS@GMAIL.COM>
2017-03-20 09:59:06 -0400

    Added componentWillMount

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -9,8 +9,29 @@ class App extends React.Component {
 
   }
 
-  render() {
+  componentWillMount() {
+      fetch(`/api/v1/notes/${this.props.noteId}.json`,
+        { method: 'get',
+          credentials: 'include'
+        })
+        .then(response => {
+          if (response.ok) {
+            return response;
+          } else {
+            let errorMessage = `${response.status} ($response.statusText)`,
+              error = new Error(errorMessage);
+            throw(error);
+          }
+        })
+        .then(response => response.json())
+        .then(body => {
+          // let song = body.tab;
+          // this.setState({ song: song});
+                        })
+        .catch(error => console.error(`Error in fetch: ${error.message}`));
+    }
 
+  render() {
 
     return(
       <div className="animated fadeIn">
